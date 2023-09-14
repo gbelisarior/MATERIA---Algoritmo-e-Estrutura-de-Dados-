32082557e5f38be7a0c63b651d380510c248c15a
Gabriel Belisário <142430065+gbelisarior@users.noreply.github.com>
2023-09-14 14:08:23 -0300

    Update seculo a partir do ano_atividade 2.cpp

--- seculo a partir do ano_atividade 2.cpp
+++ seculo a partir do ano_atividade 2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-
+//codigo em C++ que verifica qual seculo de acordo com o ano informado. 
         using namespace std;
 
         int solution(int ano) {
         
         	int seculo = (ano + 99) / 100;
-        	cout << "O ano " << "se encontra no seculo  " << seculo << endl;
+        	cout << " \n O ano: " << " \n se encontra no seculo:  " << seculo << endl;
         	
         	return seculo;
         }
